d3a21109143053914e6abf4feaf134c9190c70c7
Arthur <arthurcarvalhomelo22@gmail.com>
2025-11-20 17:33:09 -0300

    Removendo a classe "Executar" e atribuindo o top-level statements

--- Calculadora.P1/Models/Calculadora .cs
+++ Calculadora.P1/Models/Calculadora .cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
-namespace Projeto1.Models
+namespace Calculadora_P1.Models
 {
     public class Calculadora
     {
